767767de21786e888ce7f68bfecd980236450bd9
mdhussain-28 <106447909+mdhussain-28@users.noreply.github.com>
2024-06-04 20:50:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        appendAlert('Nice, you triggered this alert message!', 'success')
+    })
+}
+
+
+function submitForm() {
+    // Your form submission logic here (e.g., sending form data to server)
+    // For demonstration, we'll simply show an alert
+    alert("You have submitted the form!");
+    // Optionally, you can reset the form after submission
+    document.getElementById("contactForm").reset();
+}
